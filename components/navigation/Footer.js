418bfa357c9ad5ead249828c99f7bc1130d9102a
umangcodes <umanga.eng@gmail.com>
2024-01-06 16:46:37 -0500

    feat: css for mobiles

--- components/navigation/Footer.js
+++ components/navigation/Footer.js
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <div className="flex justify-around px-24 pt-24 pb-48">
-        <div className="flex flex-col gap-2">
+    <div className="flex justify-around px-24 pt-24 pb-48 flex-col md:flex-row">
+        <div className="flex flex-col gap-2 pb-10 sm:pb-0">
           <h1 className="text-accent1 text-2xl font-semibold">Contact Us</h1>
           <p className="text-secondary/90">Address</p>
           <p className="text-secondary/90">Contact number</p>
           <p className="text-secondary/90">Email</p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col pb-10 sm:pb-0">
           <h1 className="text-accent1 text-2xl font-semibold pb-2">Schedule</h1>
           <ul className="text-secondary/90 flex flex-col gap-2">
             <li>Monday		10:00 - 20:00</li>
@@ -21,9 +21,9 @@ const Footer = () => {
         <div className="flex flex-col">
           <h1 className="text-accent1 text-2xl font-semibold">Subscribe</h1>
           <h2 className="text-accent1 text-lg pb-2">Subscribe to stay in touch</h2>
-          <fieldset className="border border-secondary bg-opacity-0">
+          <fieldset className="border border-secondary bg-opacity-0 w-full sm:w-auto">
             <input placeholder="Your Email" style={{ background: 'transparent' }} className="outline-none text-secondary px-5 py-2"/>
-            <button className="px-3 py-3 mx-1 my-1 bg-secondary font-semibold text-lg ">Subscribe</button>
+            <button className="px-3 py-3 mx-1 my-1 bg-secondary font-semibold text-lg sm:px-2">Subscribe</button>
           </fieldset>
         </div>
     </div>
