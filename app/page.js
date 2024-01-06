cb2bd532388a0488e703d0c4969749e2f258900a
umangcodes <umanga.eng@gmail.com>
2024-01-06 16:30:30 -0500

    team banner

--- app/page.js
+++ app/page.js
@@ -7,13 +7,14 @@ import BookToday from "@/components/banners/BookToday";
 import BrandRoller from "@/components/banners/BrandRoller";
 import Specials from "@/components/banners/Specials";
 import Footer from "@/components/navigation/Footer";
-
+import Team from "@/components/banners/Team"
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center">
       <div className='w-full'>
         <Welcome />
         <Intro />
+        <Team />
         <ServiceCards />
         <Specials />
         <ProductCards />
