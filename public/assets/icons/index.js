e44688abddf74b8d31138d89654f3e5932271a61
umangcodes <umanga.eng@gmail.com>
2024-01-06 16:18:00 -0500

    feat: images and icons indexed

---
+++ public/assets/icons/index.js
@@ -0,0 +1,8 @@
+import TIKTOK from "./tiktok.png"
+import FACEBOOK from "./facebook.png"
+import INSTA from "./instagram.png"
+import CHECK from "./checkmark.png"
+
+
+
+export{TIKTOK, FACEBOOK, INSTA, CHECK}
---
+++ public/assets/images/index.js
@@ -0,0 +1,8 @@
+import LANDING from "./landingpage.jpg"
+import INTRO1 from "./intro1.jpg"
+import INTRO2 from "./intro2.jpg"
+import BRAND1 from "./brand1.png"
+import BRAND2 from "./brand2.png"
+import LOGO from "./logo.jpg"
+
+export {LANDING, INTRO1, INTRO2, BRAND1, BRAND2, LOGO}
