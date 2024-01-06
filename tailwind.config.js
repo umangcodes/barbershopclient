de1a42a0ff5305e195f460ed4c17769b8eef59cc
umangcodes <umanga.eng@gmail.com>
2024-01-06 16:18:55 -0500

    feat: tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#4F83CC', // A brighter, more vivid blue
+        secondary: '#D1B06B', // A brighter, more vibrant gold
+        accent1: '#A3B4A2', // A muted, calming green
+        accent2: '#D9D0C1', // A soft, neutral beige
+        background: '#1A202C', // A deep, dark charcoal for the background
+        textPrimary: '#FFFFFF', // Crisp, clear white for primary text
+        textSecondary: '#CCCCCC', // Lighter grey for secondary text
+        highlight: '#9C3D54', // A rich, deep maroon for highlights
+        muted: '#606F7B'
+      },
+      listStyleImage: {
+        checkmark: 'url("/assets/icons/checkmark.png")',
       },
     },
   },
